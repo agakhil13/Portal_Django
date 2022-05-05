04ac100cc2bdd42fb95f0756254d2897367bae81
agakhil13 <aguptaji1310@gmail.com>
2022-05-05 08:54:37 +0530

    Homework

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,4 @@
+from pyexpat import model
 from tabnanny import verbose
 import django
 from django.db import models
@@ -16,3 +17,13 @@ class Notes(models.Model):
     class Meta():
         verbose_name = 'notes'
         verbose_name_plural = 'notes'
+
+class Homework(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    subject = models.CharField(max_length=50)
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    due = models.DateField()
+    is_finished = models.BooleanField(default=False)
+    def __str__(self):
+        return self.subject
