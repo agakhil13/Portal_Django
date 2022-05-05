3420244b26fa790b53bbb938bdab807af92bbeba
agakhil13 <aguptaji1310@gmail.com>
2022-05-05 17:35:00 +0530

    todo

--- dashboard/models.py
+++ dashboard/models.py
@@ -27,3 +27,12 @@ class Homework(models.Model):
     is_finished = models.BooleanField(default=False)
     def __str__(self):
         return self.subject
+
+
+class Todo(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    is_finished = models.BooleanField(default=False)
+
+    def __str__(self) -> str:
+        return self.title
