03915806552e433fb876234ed176c6ef1366fdb9
agakhil13 <aguptaji1310@gmail.com>
2022-05-07 10:16:20 +0530

    Profile

--- studentstudyportal/settings.py
+++ studentstudyportal/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-hk+6gz+_#-%97m%_r*w05p*+3gqyt1c(85#1tx-sj$6loied6v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -124,8 +125,11 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS=[BASE_DIR/"static"]
+STATIC_URL = 'static/'
+STATICFILES_DIRS=[
+    os.path.join(BASE_DIR,'static')
+]
+STATIC_ROOT = os.path.join(BASE_DIR,'assets')
 
 LOGIN_REDIRECT_URL = 'home'
 LOGIN_URL = 'login'
