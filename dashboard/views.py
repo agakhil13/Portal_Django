1412b8ca8b299e6dc82a4c0a2fdb2a3f27f4dd60
agakhil13 <aguptaji1310@gmail.com>
2022-05-07 09:19:38 +0530

    connect login and register

--- dashboard/views.py
+++ dashboard/views.py
@@ -327,7 +327,7 @@ def register(request):
             form.save()
             username = form.cleaned_data.get('username')
             messages.success(request,f"Account Created for {username}!!")
-            #return redirect('login')
+            return redirect('login')
     else:
         form = UserRegistrationForm()
     context = {
